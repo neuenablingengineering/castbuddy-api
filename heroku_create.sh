590f2d784b4cb5468781581f47f3279164d5deee
Ben Faucher <faucher.benp@gmail.com>
2018-06-26 15:19:29 -0400

    Adding check for if heroku app is already created; attaches to existing app if so

--- heroku_create.sh
+++ heroku_create.sh
@@ -2,4 +2,9 @@
 
 heroku create cbapi-$CI_ENVIRONMENT_SLUG
 
+# if doesn't succeed, manually add git remote
+if [ $? -ne 0 ]; then
+  heroku git:remote --app=cbapi-$CI_ENVIRONMENT_SLUG
+fi
+
 exit 0
