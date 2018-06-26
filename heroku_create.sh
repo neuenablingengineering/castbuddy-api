f0dd55e763a3d043685a58a805b4eb5a58e71c05
Ben Faucher <faucher.benp@gmail.com>
2018-06-26 15:45:03 -0400

    Still tweaking; git just does not want to push to heroku

--- heroku_create.sh
+++ heroku_create.sh
@@ -3,8 +3,8 @@
 heroku create cbapi-$CI_ENVIRONMENT_SLUG
 
 # if doesn't succeed, manually add git remote
-if [ $? -ne 0 ]; then
-  heroku git:remote --app=cbapi-$CI_ENVIRONMENT_SLUG
-fi
+#if [ $? -ne 0 ]; then
+#  heroku git:remote --app=cbapi-$CI_ENVIRONMENT_SLUG
+#fi
 
 exit 0
