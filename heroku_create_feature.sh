2c669a4a09dd7ca91faad1269051a2a551953699
Ben Faucher <faucher.benp@gmail.com>
2018-06-26 22:15:53 -0400

    FINALLY got the first-heroku-commit problem solved. Now adding in the ability to close out a feature environment from the GitLab web interface.

--- heroku_create_feature.sh
+++ heroku_create_feature.sh
@@ -3,8 +3,9 @@
 heroku create cbapi-$CI_ENVIRONMENT_SLUG
 
 # if doesn't succeed, manually add git remote
-if [ $? -ne 0 ]; then
-  heroku git:remote --app=cbapi-$CI_ENVIRONMENT_SLUG
-fi
+#if [ $? -ne 0 ]; then
+#  heroku git:remote --app=cbapi-$CI_ENVIRONMENT_SLUG
+#fi
 
+# Force exit 0 in case feature has already been deployed to heroku
 exit 0
